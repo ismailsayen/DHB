466129d1890aeb1acd4b57390d3d3d6119839d6c
ismail sayen <sayensma02@gmail.com>
2024-04-16 17:39:58 +0100

    sgidi ikhana

--- projectHDB/src/components/Caroussel.jsx
+++ projectHDB/src/components/Caroussel.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import '../styles/caroussel.css';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const pictures = [
     { url: "neon1.png" },
-    { url: "neon2.png" },
+    { url: "outako1.png" },
     { url: "neon3.png" },
-    { url: "outako1.png" },
-    { url: "outako1.png" },
+    { url: "sport2.png" },
+    { url: "outako2.png" },
+    { url: "neon2.png" },
     { url: "sport1.png" },
-    { url: "sport1.png" }
 ];
 function Caroussel() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    };
     return (
         <div className='caross-con'>
             <div className='parent-caross'>
                 <div className='title'>
                     <h1>NOS RÉALISATIONS</h1>
                 </div>
-                <div>
-                    hh
+                <div className='images-con'>
+                    <Slider {...settings} >
+                        {
+                            pictures.map((ele, ind) =>
+                                <img src={`./images/${ele.url}`} width={'400'} height={'400'} className='img'/>
+                            )}
+                    </Slider>
                 </div>
             </div>
         </div>
