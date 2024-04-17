7d8330dd5571a65dacf66519c72d0f683225d4b8
ismail sayen <sayensma02@gmail.com>
2024-04-17 16:41:48 +0100

    zerza l header na

--- projectHDB/src/components/Header.jsx
+++ projectHDB/src/components/Header.jsx
@@ -5,8 +5,9 @@ export default function Header() {
             <img src='/logo.png' width={'140'} height={'100'} />
             <div className='links'>
                 <ul>
-                    <li><a href='#'>Pack</a></li>
+                    <li><a href='#'>Pack 1</a></li>
                     <li><a href='#'>Projets</a></li>
+                    <li><a href='#'>Pack 2</a></li>
                 </ul>
             </div>
         </header>
