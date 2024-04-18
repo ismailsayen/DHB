23c216bb33909413abb3982bd6f39870dacb5759
ismail sayen <sayensma02@gmail.com>
2024-04-18 18:00:29 +0100

    lmohkil n lizimaje i3dl

--- projectHDB/src/components/Caroussel.jsx
+++ projectHDB/src/components/Caroussel.jsx
@@ -1,40 +1,17 @@
 import '../styles/caroussel.css';
-import Slider from 'react-slick';
-import { useState, useEffect } from 'react';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { transform } from 'framer-motion';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 const pictures = [
-    { url: "neon1.png" },
-    { url: "outako1.png" },
-    { url: "neon3.png" },
-    { url: "sport2.png" },
-    { url: "outako2.png" },
-    { url: "neon2.png" },
-    { url: "sport1.png" },
+    { url: "neon.png", link: "https://neonunivers.com/" },
+    { url: "outako.png", link: "https://shonensports.com/" },
+    { url: "sport.png", link: "https://quedufootcom.com/" },
 ];
+
 function Caroussel() {
-    const [number, setNumber] = useState(0);
-
-    const handleResize = () => {
-        setNumber(window.innerWidth > 500 ? 3 : 1);
-    };
-
-    useEffect(() => {
-        handleResize();
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: number,
-        slidesToScroll: 1
-    };
     return (
         <div className='carousel-container' id='projet'>
             <div className='parent-caross'>
@@ -42,12 +19,23 @@ function Caroussel() {
                     <h1>NOS RÉALISATIONS</h1>
                 </div>
                 <div className='images-con'>
-                    <Slider {...settings} >
-                        {
-                            pictures.map((ele, ind) =>
-                                <img key={ind} src={`./images/${ele.url}`} width={'400'} height={'400'} className='img' />
-                            )}
-                    </Slider>
+                    <Swiper
+                        modules={[Navigation]}
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        onSlideChange={() => console.log('slide change')}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        navigation
+                    >
+                        {pictures.map((picture, index) => (
+                            <SwiperSlide>
+                                <a href={picture.link} target="_blank" rel="noopener noreferrer">
+                                    <img src={`./images/${picture.url}`} key={index} alt={`Image ${index + 1}`} />
+                                </a>
+                            </SwiperSlide>
+
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </div>
